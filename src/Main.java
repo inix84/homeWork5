19240352380df3c3977341ca03cbc9913660b2d6
inix84 <inix84@mail.ru>
2022-09-12 00:20:52 +0500

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,13 @@ public class Main {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
 
         }
+
+        int year = 2004;
+        if (year % 4==0 && year%100!=0) {
+            System.out.println(year+" год является високосным");
+        } else {
+            System.out.println(year+" год не является високосным");
+        }
+
     }
 }
