d8b9244a7a36e6810ffe00c3b3819ba42c1e011f
inix84 <inix84@mail.ru>
2022-09-12 00:12:58 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int clientOS = 0;
-        if (clientOS == 0) {
+        int clientDeviceYear = 2015;
+
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для Android  по ссылке");
+            } else if (clientOS == 1 && clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
         }
     }
+}
